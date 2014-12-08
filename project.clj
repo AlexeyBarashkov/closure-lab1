b6e58e9b5641a23952ad849e7dcbd0e58bfabfce
Alexey Barashkov <alexey.barashkov@strevus.com>
2014-12-08 20:44:13 +0300

    added example files, added arguments check

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
-  :main first-lab.core)
+  :main barashkov.first-lab.core)
--- src/first_lab/core.clj
+++ src/first_lab/core.clj
@@ -1,8 +1,27 @@
-(ns first-lab.core)
+(ns barashkov.first-lab.core
+  (:require [clojure.java.io :as io]
+    [clojure.string :as str]))
 
-(defn a [] (println "ololo"))
+
+(def ^:private distance-calculation-types #{"euclidean" "hamming"})
+
+
+(defn- check-arguments
+  [distance-calculation-type file-path]
+  (reduce
+    (fn [memo expression]
+      (and memo expression))
+    true
+    [(not (str/blank? distance-calculation-type))
+     (not (str/blank? file-path))
+     (contains? distance-calculation-types distance-calculation-type)
+     (.exists (io/file file-path))]))
 
 (defn -main
-  "I don't do a whole lot."
-  []
-  (println "Hello, World!"))
+  [distance-calculation-type file-path]
+  (if (check-arguments distance-calculation-type file-path)
+    (do
+      (println "Distance type:" distance-calculation-type)
+      (println "Source file:" file-path))
+    (println "Wrong arguments"))
+  )
